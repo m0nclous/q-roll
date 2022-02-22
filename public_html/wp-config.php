d819c939741a99d1b575baac1cf50613299e7d5e
m0nclous <m0nclous@vk.com>
2022-02-22 15:39:03 +0300

    Добавлен разделитель

--- public_html/wp-config.php
+++ public_html/wp-config.php
@@ -52,7 +52,7 @@ $table_prefix = 'wp_';
 define('WP_DEBUG', false);
 
 /** Кэширование Redis */
-define('WP_CACHE_KEY_SALT', 'q-roll.ru');
+define('WP_CACHE_KEY_SALT', 'q-roll.ru:');
 define('WP_CACHE', true);
 
 
