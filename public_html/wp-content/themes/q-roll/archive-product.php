e35c4ba6e303b80fcfc7ec831a6d7f624b975bac
m0nclous <m0nclous@vk.com>
2022-03-11 14:38:53 +0300

    Добавлен архив таксономии

--- public_html/wp-content/themes/q-roll/archive-product.php
+++ public_html/wp-content/themes/q-roll/archive-product.php
@@ -15,12 +15,21 @@
 		</ul>
 	</div>
 
-	<?php foreach ($categories as $category) : ?>
+	<?php if (is_tax()) : ?>
+		<?php $category = get_queried_object() ?>
+
 		<div class="products-container" id="<?= $category->slug ?>">
 			<h1><?= $category->name ?></h1>
 			<?= do_shortcode('[products per_page="-1" category="' . $category->slug . '" columns="4" orderby="menu_order" order="asc"]') ?>
 		</div>
-	<?php endforeach ?>
+	<?php else : ?>
+		<?php foreach ($categories as $category) : ?>
+			<div class="products-container" id="<?= $category->slug ?>">
+			<h1><a href="<?= get_term_link($category) ?>"> <?= $category->name ?></a></h1>
+				<?= do_shortcode('[products per_page="-1" category="' . $category->slug . '" columns="4" orderby="menu_order" order="asc"]') ?>
+			</div>
+		<?php endforeach ?>
+	<?php endif ?>
 </main>
 
 <?php get_footer() ?>
