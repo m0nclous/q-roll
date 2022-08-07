9b52a5f6bcb74f3b4d0653b48d6a109ff9a1df68
m0nclous <m0nclous@vk.com>
2022-08-07 23:49:27 +0300

    Add YouKassa plugin

---
+++ public_html/wp-content/plugins/yookassa/includes/SucceededNotificationChecker.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Yoomoney\Includes;
+
+use Yookassa\Model\Notification\NotificationSucceeded;
+
+class SucceededNotificationChecker
+{
+    /** @var PaymentsTableModel */
+    private $paymentsTableModel;
+
+    public function __construct($paymentsTableModel)
+    {
+        $this->paymentsTableModel = $paymentsTableModel;
+    }
+
+    public function isHandled(NotificationSucceeded $notification)
+    {
+        $paymentId = $notification->getObject()->getId();
+
+        return $this->paymentsTableModel->isPaymentPaid($paymentId)
+                && $this->paymentsTableModel->isPaymentCaptured($paymentId);
+    }
+}
---
+++ public_html/wp-content/plugins/yookassa/includes/lib/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '80b9803ca366f951f6f8f1c04c47955733af26e9',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '80b9803ca366f951f6f8f1c04c47955733af26e9',
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => '1.1.4',
+            'version' => '1.1.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+            'dev_requirement' => false,
+        ),
+        'yoomoney/yookassa-sdk-php' => array(
+            'pretty_version' => '2.4.2',
+            'version' => '2.4.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yoomoney/yookassa-sdk-php',
+            'aliases' => array(),
+            'reference' => 'feff2bd0450a8fbedbb09b3f0caab203478555a7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
