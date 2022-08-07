54fef5b20d43d2e1901c8b54b693ee4806253f17
m0nclous <m0nclous@vk.com>
2022-08-08 01:58:41 +0600

    Рефакторинг

--- public_html/wp-content/themes/q-roll/functions.php
+++ public_html/wp-content/themes/q-roll/functions.php
@@ -2,7 +2,7 @@
 
 add_action('wp_enqueue_scripts', function () {
 	$version = wp_get_theme()->get('Version');
-	$js = fn ($file) => get_theme_file_uri($file);
+//	$js = fn ($file) => get_theme_file_uri($file);
 
 	wp_dequeue_style('wc-blocks-style');
 	wp_dequeue_style('wp-block-library');
@@ -19,11 +19,8 @@ add_action('wp_enqueue_scripts', function () {
 /** Удаляем jquery-migrate */
 add_action('wp_default_scripts', fn ($scripts) => (!is_admin() && isset($scripts->registered['jquery']) && $scripts->registered['jquery']->deps) ? $scripts->registered['jquery']->deps = array_diff($scripts->registered['jquery']->deps, ['jquery-migrate']) : null);
 
-/** Добавляем preconnect для гугл шрифтов */
+/** Добавляем preconnect для Google Fonts */
 add_action('wp_head', fn () => print('<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>'));
-
-/** Добавляем meta тег для верификации enot */
-add_action('wp_head', fn () => print('<meta name="enot" content="8471646207409BHdzxmnzXMMo1pz-cI-yhGdbnbE6gnPV">'));
 
 /** Добавляем meta тег для верификации google */
 add_action('wp_head', fn () => print('<meta name="google-site-verification" content="zt5uYkc802kL5CJY4ul_leUNvwFywnn1CUx55xIXEzI" />'));
@@ -34,7 +31,7 @@ add_action('wp_head', fn () => print('<meta name="yandex-verification" content="
 /** Yandex.Metrika counter */
 if (user_agent_is_user()) add_action('wp_head', fn () => print('<script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(87871205, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true, ecommerce:"dataLayer" }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/87871205" style="position:absolute; left:-9999px;" alt="" /></div></noscript>'));
 
-/** Эта функция позволит плагинам и темам изменять метатег <title> */
+/** Эта функция позволит плагинам и темам изменять мета-тег <title> */
 add_theme_support('title-tag');
 
 /** Включает поддержку html5 разметки для списка комментариев, формы комментариев, формы поиска, галереи и т.д. */
@@ -43,7 +40,7 @@ add_theme_support('html5', ['comment-list', 'comment-form', 'search-form', 'gall
 /** Включает поддержку WooCommerce */
 add_theme_support('woocommerce');
 
-/** Удаление кнопки WP в админ панеле */
+/** Удаление кнопки WP в админ панели */
 add_action('admin_bar_menu', fn ($wp_admin_bar) => $wp_admin_bar->remove_node('wp-logo'), 999);
 
 /** Добавляем области меню */
@@ -103,7 +100,7 @@ add_filter('site_status_tests', function ($tests) {
 });
 
 /** Исправляем ошибку "префикс article неизвестен валидатору" */
-add_filter('language_attributes', fn ($lang) => $lang .= ' prefix="article: http://ogp.me/ns/article#" ', $lang);
+add_filter('language_attributes', fn ($lang) => $lang .= ' prefix="article: https://ogp.me/ns/article#" ', $lang);
 
 /** Убираем хлебные крошки woocommerce */
 remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
@@ -114,11 +111,11 @@ add_action('woocommerce_product_thumbnails_columns', fn () => 1);
 /** Убираем в Yoast Seo разметку поиска */
 add_filter('disable_wpseo_json_ld_search', '__return_true');
 
-remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10);
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_rating');
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50);
 
-remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
+remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs');
 remove_action('woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15);
 remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
 
@@ -127,7 +124,8 @@ add_action('woocommerce_single_product_summary', 'the_content', 5);
 
 add_action('woocommerce_single_product_summary', fn () => printf('<div class="product__weight">%s</div>', get_the_weight(with_unit: true)), 7);
 
-function get_the_weight($with_unit = false) {
+function get_the_weight($with_unit = false): string
+{
 	global $product;
 	if (empty($product->get_weight())) return '';
 	return $with_unit ? ($product->get_weight() . ' ' . get_option('woocommerce_weight_unit')) : $product->get_weight();
@@ -138,6 +136,7 @@ add_filter('wp_nav_menu_objects', function ($items) {
 	return $items;
 });
 
-function user_agent_is_user() {
-	return strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome-Lighthouse') === false && strpos($_SERVER['HTTP_USER_AGENT'], 'YandexBot') === false;
+function user_agent_is_user(): bool
+{
+	return !str_contains($_SERVER['HTTP_USER_AGENT'], 'Chrome-Lighthouse') && !str_contains($_SERVER['HTTP_USER_AGENT'], 'YandexBot');
 }
